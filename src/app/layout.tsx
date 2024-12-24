5132310020e383127995a5d353acbfec14dacfec
silverseacode <businessfranconicolascoronel94@gmail.com>
2024-12-24 14:48:58 -0300

    name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import { Poppins } from 'next/font/google'
 const popinsFont = Poppins({ weight: ['400', '600', '700'], subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Bank Website | TarreDev 💸 ',
-  description: 'Follow me for more!',
+  title: 'Bank Website',
+  description: 'Bank Landing',
 }
 
 export default function RootLayout({
